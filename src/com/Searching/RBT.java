6b98d98ff22fb338588c7032b21db7187c1c383e
Yogesh Rajak <yrajak9@gmail.com>
2021-09-06 11:44:21 +0530

    rotation count done!

--- src/com/Searching/RBT.java
+++ src/com/Searching/RBT.java
@@ -2,7 +2,7 @@ package com.Searching;
 
 public class RBT {
     public static void main(String[] args) {
-        int[] arr = {4,5,6,7,0,1,2};
+        int[] arr = {4,5,6,6,7,0,1,2};
         int target = 6;
          System.out.println(Search(arr, target));
     }
@@ -47,10 +47,27 @@ public class RBT {
                 return mid;
             } else if (mid > start && nums[mid] < nums[mid - 1]) {
                 return mid - 1;
-            } else if (nums[mid] <= nums[start]) {
-                end = mid - 1;
-            } else {
+            }
+            if (nums[mid] == nums[start] && nums[mid] == nums[end]) {
+                // skip the duplicates
+
+                // check if start is pivot
+                if (start < end && nums[start] > nums[start + 1]) {
+                    return start;
+                }
+                start++;
+
+                // check whether end is pivot
+                if (end > start && nums[end] < nums[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+            }
+            // left side is sorted, so pivot should be in right
+            else if(nums[start] < nums[mid] || (nums[start] == nums[mid] && nums[mid] > nums[end])) {
                 start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
         return -1;
