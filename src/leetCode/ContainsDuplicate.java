167c861fd896128203f35f2f3a12eec28e0e68fa
Yogesh Rajak <yrajak9@gmail.com>
2021-09-07 15:36:35 +0530

    Duplicate element in array

---
+++ src/leetCode/ContainsDuplicate.java
@@ -0,0 +1,33 @@
+package leetCode;
+
+import java.util.Arrays;
+
+//Q.217
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+    int[] nums = {1,2,3,4};
+        System.out.println(containsDuplicate(nums));
+    }
+    static boolean containsDuplicate(int[] nums) {
+
+        //#1st approach
+
+//    for (int i=0; i<nums.length-1; i++ ){
+//        for (int j=i+1; j<nums.length-1; j++){
+//            if (nums[i] == nums[j])
+//                return true;
+//        }
+//    }
+//    return false;
+        //passed 20/20 Test cases but Time limit Exceeded
+
+        //#2nd approach
+        Arrays.sort(nums);
+        for (int i=0; i<nums.length-1; i++ ){
+            if (nums[i] == nums[i+1])
+                return true;
+        }
+        return false;
+    }
+
+}
